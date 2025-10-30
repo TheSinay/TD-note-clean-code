ba8d138d8f49328a6a670668aba155fd48db838d
TheSinay <darius.brice1@gmail.com>
2025-10-30 14:34:44 +0100

    ajout du code pour le carre

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ export const isBrelan = (dices) => {
     }
     return false;
 };
+
+
+export const isCarre = (dices) => {
+    if (!Array.isArray(dices) || dices.length === 0) return false;
+    
+    dices.sort((a, b) => a - b);
+    let currentCount = 1;
+    for (let i = 1; i < dices.length; i++) {
+        if (dices[i] === dices[i - 1]) {
+            currentCount++;
+            if (currentCount == 4) return true;
+        } else {
+            currentCount = 1;
+        }
+    }
+    return false;
+};
+
+
