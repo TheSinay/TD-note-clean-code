a6eaacac10f638bf4e992f4e92daffa6549480d3
TheSinay <darius.brice1@gmail.com>
2025-10-30 14:30:44 +0100

    ajout du code dans  isBrelan

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const isBrelan = (dices) => {
-    return;
+export const isBrelan = (dices) => {
+    if (!Array.isArray(dices) || dices.length === 0) return false;
+    
+    dices.sort((a, b) => a - b);
+    let currentCount = 1;
+    for (let i = 1; i < dices.length; i++) {
+        if (dices[i] === dices[i - 1]) {
+            currentCount++;
+            if (currentCount == 3) return true;
+        } else {
+            currentCount = 1;
+        }
+    }
+    return false;
 };
