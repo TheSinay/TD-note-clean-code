49f09eee263923d0f3b488022afe97f673dba555
TheSinay <darius.brice1@gmail.com>
2025-10-30 14:14:43 +0100

    ajout de test pour le brelan

--- index.test.js
+++ index.test.js
@@ -6,4 +6,20 @@ describe("isBrelan",()=>{
         const result = isBrelan([2,2,2,4,5]);
         expect(result).toBe(true);
     });
+    it("should return false for [1,2,3,4,5]",()=>{
+        const result = isBrelan([1,2,3,4,5]);
+        expect(result).toBe(false);
+    });
+    it("should return true for [6,6,6,6,1]",()=>{
+        const result = isBrelan([6,6,6,6,1]);
+        expect(result).toBe(true);
+    });
+    it("should return false for [3,3,4,4,5]",()=>{
+        const result = isBrelan([3,3,4,4,5]);
+        expect(result).toBe(false);
+    });
+    it("should return true for [5,5,5,5,5]",()=>{
+        const result = isBrelan([5,5,5,5,5]);
+        expect(result).toBe(true);
+    });
 });
