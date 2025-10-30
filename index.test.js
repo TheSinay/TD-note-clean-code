0db02ca598242e19f5d98022abd9a6254547c9a4
TheSinay <darius.brice1@gmail.com>
2025-10-30 14:12:41 +0100

    creation d u test pour les brelans

--- index.test.js
+++ index.test.js
@@ -1,1 +1,9 @@
 import { describe,it,expect } from "vitest";
+import {isBrelan} from "./index.js";
+
+describe("isBrelan",()=>{
+    it("should return true for [2,2,2,4,5]",()=>{
+        const result = isBrelan([2,2,2,4,5]);
+        expect(result).toBe(true);
+    });
+});
