189c0292ae7b73c3b6937e97d8c5266545cec4e5
TheSinay <darius.brice1@gmail.com>
2025-10-30 14:49:59 +0100

    refacto du code

--- index.js
+++ index.js
@@ -1,67 +1,36 @@
-export const isBrelan = (dices) => {
-    if (!Array.isArray(dices) || dices.length === 0) return false;
+
+const cheackDicesResult = (dices, repeat) => {
+    if (!Array.isArray(dices) || dices.length === 0 || repeat ==0) return false;
     
     dices.sort((a, b) => a - b);
     let currentCount = 1;
     for (let i = 1; i < dices.length; i++) {
         if (dices[i] === dices[i - 1]) {
             currentCount++;
-            if (currentCount == 3) return true;
+            if (currentCount == repeat) return dices[i];
         } else {
             currentCount = 1;
         }
     }
-    return false;
+    return 0;
+};
+
+export const isBrelan = (dices) => {
+    return cheackDicesResult(dices, 3) !== 0;
 };
 
 
 export const isCarre = (dices) => {
-    if (!Array.isArray(dices) || dices.length === 0) return false;
-    
-    dices.sort((a, b) => a - b);
-    let currentCount = 1;
-    for (let i = 1; i < dices.length; i++) {
-        if (dices[i] === dices[i - 1]) {
-            currentCount++;
-            if (currentCount == 4) return true;
-        } else {
-            currentCount = 1;
-        }
-    }
-    return false;
+    return cheackDicesResult(dices, 4) !== 0;
 };
 
 
 export const isPair = (dices) => {
-    if (!Array.isArray(dices) || dices.length === 0) return false;
-    
-    dices.sort((a, b) => a - b);
-    let currentCount = 1;
-    for (let i = 1; i < dices.length; i++) {
-        if (dices[i] === dices[i - 1]) {
-            currentCount++;
-            if (currentCount == 2) return true;
-        } else {
-            currentCount = 1;
-        }
-    }
-    return false;
+    return cheackDicesResult(dices, 2) !== 0;
 };
 
 export const isYams = (dices) => {
-    if (!Array.isArray(dices) || dices.length === 0) return false;
-    
-    dices.sort((a, b) => a - b);
-    let currentCount = 1;
-    for (let i = 1; i < dices.length; i++) {
-        if (dices[i] === dices[i - 1]) {
-            currentCount++;
-            if (currentCount == 5) return true;
-        } else {
-            currentCount = 1;
-        }
-    }
-    return false;
+    return cheackDicesResult(dices, 5) !== 0;
 };
 
 
